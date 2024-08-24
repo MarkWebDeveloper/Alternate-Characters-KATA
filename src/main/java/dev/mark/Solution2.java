2b6364d5c54b61edf7d680a4d31c4fb9bb101e29
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-08-24 18:55:22 +0200

    the solution2 is now shorter

--- src/main/java/dev/mark/Solution2.java
+++ src/main/java/dev/mark/Solution2.java
@@ -6,22 +6,13 @@ import java.util.Set;
 
 public class Solution2 {
 
-    static char currentChar;
-    static char prevChar;
-
     public static boolean isAlt(String word) {
 
         Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
-        Set<Character> consonants = new HashSet<>(Arrays.asList('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'));
 
         for (int i = 1; i < word.length(); i++) {
-
-            currentChar = word.charAt(i);
-            prevChar = word.charAt(i - 1);
-
-            if (vowels.contains(currentChar) && consonants.contains(prevChar)) {
-                continue;
-            } else if (consonants.contains(currentChar) && vowels.contains(prevChar)) {
+            if (vowels.contains(word.charAt(i)) && !vowels.contains(word.charAt(i - 1)) 
+                || vowels.contains(word.charAt(i - 1)) && !vowels.contains(word.charAt(i))) {
                 continue;
             } else {
                 return false;
